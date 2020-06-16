772fa3a273152f2caf950279ffe98cfef9646a84
Vadim Chumak <vadim.inc@gmail.com>
2020-06-16 21:40:09 +0200

    fix npm cmd

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # curl -s https://install-node.now.sh | bash -s --
-npm i git+https://git@github.com/bloxapp/blox-live.git - g
+npm i git+https://git@github.com/bloxapp/blox-live.git -g
