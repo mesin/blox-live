3c033fa5ad01c6723cb8288ce563e2eb579f1e14
Vadim Chumak <vadim.inc@gmail.com>
2020-06-16 21:41:39 +0200

    fix index.js

--- index.js
+++ index.js
@@ -17,10 +17,10 @@ console.log(
 
 const run = async () => {
   try {
-    // console.log(chalk.blue('+ Authentication'));
-    // await aws.getAccessKey();
-    // console.log(chalk.blue('+ Environment'));
-    // await aws.setup();
+    console.log(chalk.blue('+ Authentication'));
+    await aws.getAccessKey();
+    console.log(chalk.blue('+ Environment'));
+    await aws.setup();
     console.log(chalk.blue('+ Server setup'));
     await server.setupEnv();
     console.log(chalk.green('+ Completed!'));
