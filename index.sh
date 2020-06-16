a825200c703eb54da938ab4bcb068f5857ad05a8
Vadim Chumak <vadim.inc@gmail.com>
2020-06-16 22:09:22 +0200

    improve sh script

--- index.sh
+++ index.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-# curl -s https://install-node.now.sh | bash -s --
+/usr/local/bin/node --version | grep "v" &> /dev/null 
+if [ $? != 0 ]; then
+    curl -s https://install-node.now.sh | bash -s --
+fi
 npm i git+https://git@github.com/bloxapp/blox-live.git -g
