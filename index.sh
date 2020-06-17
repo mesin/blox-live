311d5e6969f06872c66eb1629b3c9b0347745c27
Vadim Chumak <vadim.inc@gmail.com>
2020-06-17 09:33:56 +0200

    WIP sh changes for windows

--- index.sh
+++ index.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
+
+fetch() {
+    set +e
+    curl -s "https://blox-live.now.sh/install-node.sh"
+    rc=$?
+    set -e
+    if [ $rc -ne 0 ]; then
+        error "Command failed (exit code $rc): ${BLUE}${command}${NO_COLOR}"
+        exit $rc
+    fi
+}
+
+
 # install nodejs
-curl -s https://blox-live.now.sh/install-node.sh | bash -s --
+fetch | bash -s --
 # install global npm package blox-live
 npm i git+https://git@github.com/bloxapp/blox-live.git -g
