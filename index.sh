bdbda29a5003af425bfdaf728c208aaf45b21efb
Vadim Chumak <vadim.inc@gmail.com>
2020-06-17 09:02:16 +0200

    WIP sh changes for windows

--- index.sh
+++ index.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
+fetch() {
+    local command
+    set +e
+    command="curl -s https://install-node.now.sh"
+    curl -s https://install-node.now.sh
+    rc=$?
+    set -e
+    if [ $rc -ne 0 ]; then
+        error "Command failed (exit code $rc): ${BLUE}${command}${NO_COLOR}"
+        exit $rc
+    fi
+}
+
+
 /usr/local/bin/node --version | grep "v" &> /dev/null 
 if [ $? != 0 ]; then
-    curl -s https://install-node.now.sh | bash -s --
+    fetch() | bash -s --
 fi
-npm i git+https://git@github.com/bloxapp/blox-live.git -g
+# npm i git+https://git@github.com/bloxapp/blox-live.git -g
