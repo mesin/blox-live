a538d9d25da696f47e35422c57df2db87f48798f
Vadim Chumak <vadim.inc@gmail.com>
2020-06-21 15:01:22 +0200

    update sh script

--- index.sh
+++ index.sh
@@ -2,4 +2,6 @@
 
 # curl -s "https://blox-live.now.sh/install-node.sh" | bash -s -- && 
 # install nodejs and install global npm package blox-live
+npm config set prefix ~/.local
+echo 'PATH=~/.local/bin/:$PATH' >> ~/.bashrc
 npm i git+https://git@github.com/bloxapp/blox-live.git -g
