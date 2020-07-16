a9b4fc19f3c3d2cbf15d88f9853b0fe3f71d12ea
david-blox <david@blox.io>
2020-07-16 16:25:04 +0300

    change to test

---
+++ app/components/Test/Test.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Test = () => {
+  return (
+    <div>
+      <h1>Test page</h1>
+      <button onClick={() => console.log('test')}>Test</button>
+    </div>
+  );
+};
+
+export default Test;
