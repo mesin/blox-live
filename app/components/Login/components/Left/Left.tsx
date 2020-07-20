90c0cfa5170eaf1ab500a25c7be7f427c07793e5
Lior Rutenberg <liorr@blox.io>
2020-07-20 15:33:15 +0300

    fix yarn tsc

--- app/components/Login/components/Left/Left.tsx
+++ app/components/Login/components/Left/Left.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Icon } from 'common/components';
+import { Icon } from '../../../../common/components';
 
 const logo = 'assets/images/staking-logo.svg';
 
