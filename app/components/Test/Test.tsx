623ddb64474c42cf8e75178414b84935b22ebc31
Vadim Chumak <vadim.inc@gmail.com>
2020-07-21 17:39:18 +0200

    WIP CLI on UI

--- app/components/Test/Test.tsx
+++ app/components/Test/Test.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
-
-const Test = () => {
+import Configstore from 'configstore';
+import InstallService from '../../backend/proccess-manager/install.service';
+const Test = async () => {
+  const storeName = 'blox';
+  const conf = new Configstore(storeName);
+  conf.set('otp', 'test-otp');
+  conf.set('credentials', {
+    accessKeyId: 'test',
+    secretAccessKey: 'test',
+  });
+  const installService = new InstallService(storeName);
   return (
     <div>
       <h1>CLI commands</h1>
-      <button onClick={() => console.log('test')}>Install</button>
+      <button
+        onClick={async () => {
+          console.log('test');
+          await installService.run();
+        }}
+      >
+        Install
+      </button>
       <button onClick={() => console.log('test')}>Uninstall</button>
       <button onClick={() => console.log('test')}>Reinstall</button>
       <button onClick={() => console.log('test')}>Reboot</button>
