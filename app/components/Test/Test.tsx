87aec5ec86012a789d64e85c6a47c3f1d4a45d5d
Oleg Shmuelov <oleg@blox.io>
2020-07-23 17:31:08 +0300

    merge test backend

--- app/components/Test/Test.tsx
+++ app/components/Test/Test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Configstore from 'configstore';
 import InstallService from '../../backend/proccess-manager/install.service';
+import KeyVaultCli from '../../backend/key-vault/seed.service'
 import { Observer } from '../../backend/proccess-manager/observer.interface';
 import { Subject } from '../../backend/proccess-manager/subject.interface';
 
@@ -19,22 +20,32 @@ const Test = () => {
     secretAccessKey: 'test',
   });
   const installService = new InstallService(storeName);
+  const keyVaultCliService = new KeyVaultCli();
   return (
     <div>
       <h1>CLI commands</h1>
-      <button
-        onClick={async () => {
-          console.log('test');
-          const listener = new Listener();
-          installService.subscribe(listener);
-          await installService.run();
-        }}
-      >
-        Install
-      </button>
-      <button onClick={() => console.log('test')}>Uninstall</button>
-      <button onClick={() => console.log('test')}>Reinstall</button>
-      <button onClick={() => console.log('test')}>Reboot</button>
+      <div>
+        <h2>KeyVault Management</h2>
+        <button
+          onClick={async () => {
+            console.log('test');
+            const listener = new Listener();
+            installService.subscribe(listener);
+            await installService.run();
+          }}
+        >
+          Install
+        </button>
+        <button onClick={() => console.log('test')}>Uninstall</button>
+        <button onClick={() => console.log('test')}>Reinstall</button>
+        <button onClick={() => console.log('test')}>Reboot</button>
+      </div>
+      <div>
+        <h2>Seed Management</h2>
+        <button onClick={async () => { await keyVaultCliService.seedGenerate()}}>Seed</button>
+        <button onClick={async () => { await keyVaultCliService.mnemonicGenerate()}}>Mnemonic</button>
+        <button onClick={async () => { await keyVaultCliService.seedToMnemonicGenerate()}}>Seed to Mnemonic</button>
+      </div>
     </div>
   );
 };
