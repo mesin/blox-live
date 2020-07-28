f4bda2073bda1a9489a241210b549feec4c20869
Vadim Chumak <vadim.inc@gmail.com>
2020-07-28 12:02:16 +0200

    add reboot logic

---
+++ app/backend/proccess-manager/reboot.process.ts
@@ -0,0 +1,13 @@
+import AwsService from '../aws/aws.service';
+import ProcessClass from './process.class';
+
+export default class RebootProcess extends ProcessClass {
+  public readonly awsService: AwsService;
+  public readonly actions: Array<any>;
+
+  constructor(storeName: string) {
+    super();
+    this.awsService = new AwsService(storeName);
+    this.actions = [{ instance: this.awsService, method: 'rebootInstance' }];
+  }
+}
