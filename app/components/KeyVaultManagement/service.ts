5ab9e9f0747a2758f7cf07d7868b4d6fe8fc70c9
david-blox <david@blox.io>
2020-08-11 12:20:07 +0300

    fix install process - complete

--- app/components/KeyVaultManagement/service.ts
+++ app/components/KeyVaultManagement/service.ts
@@ -3,9 +3,9 @@ import InstallProcess from '../../backend/proccess-manager/install.process';
 import RebootProcess from '../../backend/proccess-manager/reboot.process';
 import ReinstallProcess from '../../backend/proccess-manager/reinstall.process';
 
-export const processInstantiator = (processName: string, payload) => {
-  const { accessKeyId, secretAccessKey, authToken } = payload;
-  if (processName === PROCESSES.INSTALL) {
+export const processInstantiator = (processName: string, payload: Record<string, any> | undefined) => {
+  if (payload && processName === PROCESSES.INSTALL) {
+    const { accessKeyId, secretAccessKey, authToken } = payload;
     return new InstallProcess({accessKeyId, secretAccessKey, authToken});
   }
   if (processName === PROCESSES.RESTART) {
