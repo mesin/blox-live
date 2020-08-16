ad16cee3ab70c2d00bf31ef430d17caaa1a75324
david-blox <david@blox.io>
2020-08-16 18:51:30 +0300

    guide

---
+++ app/components/Wizard/components/Wallet/Guide/components/SubTitle.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const SubTitle = styled.h1`
+  font-size: 16px;
+  font-weight: 900;
+  color: ${({theme}) => theme.gray800};
+`;
+
+export default SubTitle;
