b672a68e6f22391863c80ae5963e7d3745a0bfca
david-blox <david@blox.io>
2020-08-19 17:19:31 +0300

    keyvault modals - wip

---
+++ app/components/KeyVaultUpdate/KeyVaultUpdate.tsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import { SuccessModal, ReinstallingModal, FailureModal, ThankYouModal } from './components/Modals';
+
+const KeyVaultUpdate = ({onClose}: Props) => {
+  const [step, setStep] = useState(0);
+  const move1StepForward = () => setStep(step + 1);
+  const move2StepsForward = () => setStep(step + 2);
+  const contactSupport = () => { move1StepForward(); };
+  switch (step) {
+    case 0:
+      return <ReinstallingModal move1StepForward={move1StepForward} move2StepsForward={move2StepsForward} onClose={onClose} />;
+    case 1:
+      return <SuccessModal title={'Reactivating your KeyVault'} onClose={onClose} />;
+    case 2:
+      return <FailureModal title={'Troubleshooting Failed'} onClick={contactSupport} onClose={onClose} />;
+    case 3:
+      return <ThankYouModal onClose={onClose} />;
+    default:
+      return <ReinstallingModal move1StepForward={move1StepForward} move2StepsForward={move2StepsForward} onClose={onClose} />;
+  }
+};
+
+type Props = {
+  onClose: () => void;
+};
+
+export default KeyVaultUpdate;
---
+++ app/components/KeyVaultUpdate/components/DiscordText.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const DiscordText = styled.div`
+  font-size: 14px;
+  font-family:'Avenir Black';
+  color: ${({theme}) => theme.gray600};
+`;
+
+export default DiscordText;
