431e083ad8b3a77d15a3467327c8f179ab8903a2
david-blox <david@blox.io>
2020-08-23 13:35:15 +0300

    BLOXINF-715

--- app/components/KeyVaultReactivation/KeyVaultReactivation.tsx
+++ app/components/KeyVaultReactivation/KeyVaultReactivation.tsx
@@ -15,7 +15,7 @@ const KeyVaultReactivation = ({onClose}: Props) => {
     case 2:
       return <RestartingModal move1StepForward={move1StepForward} move2StepsForward={move2StepsForward} onClose={onClose} />;
     case 3:
-      return <SuccessModal onClose={onClose} />;
+      return <SuccessModal title={'KeyVault Reactivated!'} onClose={onClose} />;
     case 4:
       return (
         <ReinstallingModal title={'Reinstalling KeyVault'} description={'KeyVault still inactive. Starting the reinstall process.'}
--- app/components/Wizard/components/Validators/index.js
+++ app/components/Wizard/components/Validators/index.js
@@ -1,5 +1,4 @@
 export { default as SelectNetwork } from './SelectNetwork';
 export { default as CreateValidator } from './CreateValidator';
-export { default as ValidatorCreated } from './ValidatorCreated';
 export { default as StakingDeposit } from './StakingDeposit';
 export { default as AlmostDone } from './AlmostDone';
