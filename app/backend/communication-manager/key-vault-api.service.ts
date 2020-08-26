d6cf63ddbc2a7e56b61d3d91a4f8190d45fd1b6a
Oleg Shmuelov <oleg@blox.io>
2020-08-26 21:19:43 +0300

    remove got and use axios

--- app/backend/communication-manager/key-vault-api.service.ts
+++ app/backend/communication-manager/key-vault-api.service.ts
@@ -1,17 +1,15 @@
 import HttpService from './http.service';
-import got from 'got';
+import axios from 'axios';
 
 class KeyVaultApiService extends HttpService {
   constructor(storePrefix: string = '') {
     super(storePrefix);
   }
 
-  init = (): void => {
-    this.instance = got.extend({
-      prefixUrl: `http://${this.storeService.get('publicIp')}:8200/v1`,
-      headers: {
-        'Authorization': `Bearer ${this.storeService.get('vaultRootToken')}`
-      }
+  init = () => {
+    this.instance = axios.create({
+      baseURL: `http://${this.storeService.get('publicIp')}:8200/v1`,
+      headers: { 'Authorization': `Bearer ${this.storeService.get('vaultRootToken')}` }
     });
   };
 }
