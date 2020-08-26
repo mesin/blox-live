78dd3d90bccf99bc9f2d00425e87299eeef4de4a
david-blox <david@blox.io>
2020-08-26 13:02:01 +0300

    BLOXINF-687

--- app/components/Wizard/selectors.ts
+++ app/components/Wizard/selectors.ts
@@ -8,10 +8,6 @@ export const getWalletError = (state) => state.wizard && state.wizard.error;
 
 export const getWalletVersion = (state) => state.wizard && state.wizard.wallet && state.wizard.wallet.keyVaultVersion;
 
-export const getAccountId = (state) => state.wizard && state.wizard.account && state.wizard.account.id;
-
-export const getPublicKey = (state) => state.wizard.account && state.wizard.account.publicKey;
-
 export const getDepositData = (state) => state.wizard && state.wizard.depositData;
 
 export const getWizardFinishedStatus = (state) => state.wizard.isFinished;
