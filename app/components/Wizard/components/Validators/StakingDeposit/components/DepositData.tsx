d970c23d7b6974f54c4f249ada830cd415613793
davidmarciano <david.marciano85@gmail.com>
2020-08-27 00:20:57 +0300

    deposit info changes

---
+++ app/components/Wizard/components/Validators/StakingDeposit/components/DepositData.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import styled from 'styled-components';
+import { InfoWithTooltip, Tooltip } from 'common/components';
+import { CopyToClipboardIcon } from '../../../common';
+import { DEPOSIT_DATA } from '../constants';
+
+const Wrapper = styled.div`
+  width:100%;
+  display:flex;
+  flex-direction:column;
+`;
+
+const Row = styled.div`
+  width:100%;
+  display:flex;
+  flex-direction:row;
+  align-items:center;
+  padding-bottom:16px;
+  position:relative;
+`;
+
+const KeyText = styled.div`
+  font-size: 16px;
+  font-weight: 900;
+  color: ${({theme}) => theme.gray800};
+`;
+
+const ValueText = styled.div`
+  max-width:300px;
+  overflow:hidden;
+  text-overflow:ellipsis;
+  white-space:nowrap;
+  font-size: 13px;
+  font-weight: 500;
+  color: ${({theme}) => theme.gray800};
+  margin-right:8px;
+`;
+
+const DepositData = (props: Props) => {
+  const { depositData, onCopy } = props;
+  return (
+    <Wrapper>
+      {DEPOSIT_DATA.map((row, index) => {
+        const { label, title, moreInfo, value } = row;
+        const isTxData = label === DEPOSIT_DATA[2].label;
+        const valueText = isTxData ? depositData : value;
+        return (
+          <Row key={index}>
+            <KeyText>
+              {title}:
+              <InfoWithTooltip title={moreInfo} placement="right" />
+            </KeyText>
+            {isTxData ? (
+              <Tooltip placement={'bottom'} title={valueText}>
+                <ValueText>{valueText}</ValueText>
+              </Tooltip>
+            ) : (
+              <ValueText>{valueText}</ValueText>
+            )}
+            <CopyToClipboardIcon text={valueText} onCopy={onCopy} />
+          </Row>
+        );
+      })}
+    </Wrapper>
+  );
+};
+
+type Props = {
+  depositData: string;
+  onCopy: () => void;
+};
+
+export default DepositData;
---
+++ app/components/Wizard/components/Validators/StakingDeposit/constants.ts
@@ -0,0 +1,23 @@
+export const DEPOSIT_DATA = [
+  {
+    label: 'depositTo',
+    value: '0x07b39f4fde4a38bace212b546dac87c58dfe3fdc',
+    title: 'To Address',
+    moreInfo: 'This where your deposit transactions should be sent in order to run your validator and start staking.',
+  },
+  {
+    label: 'txData',
+    value: '',
+    title: 'To Address',
+    moreInfo: 'TX data holds additional information that is required for the transaction.',
+  },
+  {
+    label: 'amount',
+    value: '32 GoETH',
+    title: 'Amount',
+    moreInfo: 'The transaction amount refers to the amount of Ethereum required for staking. Please send the exact amount.',
+  },
+];
+
+export const INTRO_TOOLTIP_TEXT = `GoETH are test tokens needed in order to participate in the Goerli Test Network.
+  'You need at least 32 GoETH test tokens in order to stake on TestNet. GoETH have no real value!`;
