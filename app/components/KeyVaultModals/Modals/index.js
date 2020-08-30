fccd04295a8ff031ee27b4a9f438dafb7babb72d
davidmarciano <david.marciano85@gmail.com>
2020-08-30 16:30:30 +0300

    BLOXINF-747 WIP

--- app/components/KeyVaultModals/Modals/index.js
+++ app/components/KeyVaultModals/Modals/index.js
@@ -1,5 +1,6 @@
 export { default as SmallModal } from './SmallModal';
 export { default as WelcomeModal } from './WelcomeModal';
+export { default as PasswordModal } from './PasswordModal';
 export { default as RestartingModal } from './RestartingModal';
 export { default as ReinstallingModal } from './ReinstallingModal';
 export { default as SuccessModal } from './SuccessModal';
