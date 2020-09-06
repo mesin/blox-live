40849581375da93bf9412a9cd0d7271643843f0f
nivBlox <niv@blox.io>
2020-09-06 17:18:54 +0300

    validators - funcs to service

--- app/components/Dashboard/components/Validators/Validators.js
+++ app/components/Dashboard/components/Validators/Validators.js
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import {Table} from 'common/components';
 import tableColumns from './tableColumns';
-import {SortType} from '../../../../common/components/Table/components/header/Sorting';
-// import {PaginationAction} from '../../../../common/components/Table/components/Footer/Pagination';
+import {handlePageClick, handleSortClick} from 'common/components/Table/service';
+import {SortType} from 'common/constants';
 
 const Wrapper = styled.div`
   width: 100%;
@@ -24,31 +24,12 @@ const Validators = ({accounts}) => {
   const [selectedSort, setSelectedSort] = React.useState('key');
   const [sortType, setSortType] = React.useState(SortType.DESCENDING);
 
-  const onPaginationClick = (offset) => {
-    if (accounts) {
-      setPagedAccounts(accounts.slice(offset, Math.min(offset + PAGE_SIZE, accounts.length)));
-      setPaginationInfo({
-        offset,
-        pageSize: PAGE_SIZE,
-        total: accounts.length,
-      });
-    }
+  const onPageClick = (offset) => {
+    handlePageClick(accounts, offset, setPagedAccounts, setPaginationInfo, PAGE_SIZE);
   };
 
   const onSortClick = (sortKey) => {
-    if (accounts) {
-      setSelectedSort(sortKey);
-      setSortType(sortType === SortType.ASCENDING ? SortType.DESCENDING : SortType.ASCENDING);
-
-      accounts.sort((a, b) => {
-        if (sortType === SortType.DESCENDING) {
-          return a[selectedSort] < b[selectedSort] ? -1 : 1;
-        }
-        return a[selectedSort] < b[selectedSort] ? 1 : -1;
-      });
-
-      setPagedAccounts(accounts.slice(paginationInfo.offset, Math.min(paginationInfo.offset + paginationInfo.pageSize, accounts.length)));
-    }
+    handleSortClick(accounts, sortKey, setSelectedSort, setSortType, sortType, setPagedAccounts, paginationInfo);
   };
 
   return (
@@ -56,7 +37,7 @@ const Validators = ({accounts}) => {
       <Title>Validators</Title>
       <Table columns={tableColumns} data={pagedAccounts} isLoading={false} isPagination
         selectedSorting={selectedSort} sortType={sortType} onSortClick={onSortClick}
-        paginationInfo={paginationInfo} onPageClick={onPaginationClick} />
+        paginationInfo={paginationInfo} onPageClick={onPageClick} />
     </Wrapper>
   );
 };
--- app/components/Dashboard/service.ts
+++ app/components/Dashboard/service.ts
@@ -20,7 +20,6 @@ export const normalizeAccountsData = (accounts) => {
     } = account;
     const newAccount = { ...account };
 
-
     newAccount.key = {
       publicKey,
       activationTime,
@@ -74,6 +73,7 @@ const fixNumOfDigits = (summary) => {
   // eslint-disable-next-line no-restricted-syntax
   for (const [key, value] of Object.entries(summary)) {
     if (Number.isNaN(value)) { return null; }
+    // @ts-ignore
     newObject[key] = value.toFixed(2);
   }
   return newObject;
