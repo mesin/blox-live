281c7a7b7c1a89ddc72acad62bee789379b5a7c4
david-blox <david@blox.io>
2020-09-16 15:10:04 +0300

    change to 15 mins

--- app/backend/common/store-manager/store.ts
+++ app/backend/common/store-manager/store.ts
@@ -14,7 +14,7 @@ export default class Store extends BaseStore {
   private readonly encryptedKeys: Array<string> = ['keyPair', 'seed', 'credentials', 'vaultRootToken'];
   private readonly cryptoAlgorithm: string = 'aes-256-ecb';
   private cryptoKey: string;
-  private cryptoKeyTTL: number = 5; // 15 minutes
+  private cryptoKeyTTL: number = 15; // 15 minutes
   private timer: any;
   private logger: Logger;
 
