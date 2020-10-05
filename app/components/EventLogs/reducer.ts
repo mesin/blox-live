f1e09cdc28e3fdd1a2c54f3664935c368ec4aae5
david-blox <david@blox.io>
2020-10-05 12:28:41 +0300

    extract eventLogs from organization dir to eventLogs dir

---
+++ app/components/EventLogs/reducer.ts
@@ -0,0 +1,46 @@
+import produce from 'immer';
+import * as actionTypes from './actionTypes';
+import { LOGOUT } from '../CallbackPage/actionTypes';
+
+export const initialState: State = {
+  data: null,
+  isLoading: false,
+  error: null,
+  activeValidators: [],
+};
+
+/* eslint-disable default-case, no-param-reassign */
+const organizationReducer = (state = initialState, action: Action) => produce(state, (draft) => {
+  switch (action.type) {
+    case actionTypes.LOAD_EVENT_LOGS:
+      draft.isLoading = true;
+      break;
+    case actionTypes.LOAD_EVENT_LOGS_SUCCESS:
+      draft.data = action.payload;
+      draft.isLoading = false;
+      break;
+    case actionTypes.LOAD_EVENT_LOGS_FAILURE:
+      draft.error = action.payload;
+      draft.isLoading = false;
+      break;
+    case LOGOUT:
+      draft.data = initialState.data;
+      draft.isLoading = initialState.isLoading;
+      draft.error = initialState.error;
+      break;
+  }
+});
+
+type State = {
+  data: any;
+  isLoading: boolean;
+  error: string;
+  activeValidators: [],
+};
+
+type Action = {
+  type: string;
+  payload: any;
+};
+
+export default organizationReducer;
