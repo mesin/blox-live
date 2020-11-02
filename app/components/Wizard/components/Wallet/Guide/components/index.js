1b3c38c74905c198f382cd2c15135bbcd0710ec3
david-blox <david@blox.io>
2020-11-02 12:42:47 +0200

    first commit

--- app/components/Wizard/components/Wallet/Guide/components/index.js
+++ app/components/Wizard/components/Wallet/Guide/components/index.js
@@ -4,6 +4,7 @@ export { default as SubTitle } from './SubTitle';
 export { default as StepsCounter } from './StepsCounter';
 export { default as Text } from './Text';
 export { default as Image } from './Image';
+export { default as Video } from './Video';
 export { default as NavigationButtonsWrapper } from './NavigationButtonsWrapper';
 export { default as NavigationButton } from './NavigationButton';
 export { default as ContentManager } from './ContentManager';
