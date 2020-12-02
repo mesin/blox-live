794a7cd27e4520b88180b25a5a3990fcad611a63
Oleg Shmuelov <oleg@blox.io>
2020-12-02 11:11:41 +0200

    override key-vault version from config store

--- app/backend/services/version/version.service.ts
+++ app/backend/services/version/version.service.ts
@@ -1,10 +1,20 @@
 import { CatchClass } from '../../decorators';
 import BloxApi from '../../common/communication-manager/blox-api';
 import { METHOD } from '../../common/communication-manager/constants';
+import Store from '../../common/store-manager/store';
 
 @CatchClass<VersionService>()
 export default class VersionService {
+  private readonly store: Store;
+
+  constructor(storePrefix: string = '') {
+    this.store = Store.getStore(storePrefix);
+  }
+
   async getLatestKeyVaultVersion() {
+    if (this.store.exists('keyVaultTempVersion')) {
+      this.store.get('keyVaultTempVersion');
+    }
     return await BloxApi.request(METHOD.GET, 'version/key-vault');
   }
 
