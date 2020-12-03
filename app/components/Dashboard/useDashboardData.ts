741cc5d6c6e7ec93744f95e7cd2021ad9a5c6b25
david-blox <david@blox.io>
2020-12-03 17:26:35 +0200

    save last connection on refresh button click

--- app/components/Dashboard/useDashboardData.ts
+++ app/components/Dashboard/useDashboardData.ts
@@ -16,6 +16,8 @@ import eventLogsSaga from '../EventLogs/saga';
 import { keyvaultLoadLatestVersion } from '../KeyVaultManagement/actions';
 import walletSaga from '../KeyVaultManagement/saga';
 
+import { saveLastConnection } from 'common/service';
+
 const useDashboardData = () => {
   useInjectSaga({key: 'wizard', saga: wizardSaga, mode: ''});
   useInjectSaga({key: 'accounts', saga: accountSaga, mode: ''});
@@ -26,6 +28,7 @@ const useDashboardData = () => {
   const dispatch = useDispatch();
 
   const loadDashboardData = async () => {
+    await saveLastConnection();
     await dispatch(loadWallet());
     await dispatch(loadAccounts());
     await dispatch(keyvaultLoadLatestVersion());
