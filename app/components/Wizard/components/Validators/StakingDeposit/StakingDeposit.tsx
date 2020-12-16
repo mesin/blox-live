340d355d6726d260536bcd1385cd422cb7e8fdf6
nivBlox <niv@blox.io>
2020-12-16 14:58:52 +0200

    move to browser url fix

--- app/components/Wizard/components/Validators/StakingDeposit/StakingDeposit.tsx
+++ app/components/Wizard/components/Validators/StakingDeposit/StakingDeposit.tsx
@@ -89,7 +89,7 @@ const StakingDeposit = (props: Props) => {
 
   const openDepositBrowser = () => {
     const {depositTo} = depositData;
-    openExternalLink('', `${config.env.WEB_APP_URL}?network=${NETWORKS[network].metaMaskId}&public_key=${publicKey}&deposit_to=${depositTo}`)
+    openExternalLink('', `${config.env.WEB_APP_URL}/staking-deposit?network=${NETWORKS[network].metaMaskId}&public_key=${publicKey}&deposit_to=${depositTo}`)
   };
 
   if (network) {
